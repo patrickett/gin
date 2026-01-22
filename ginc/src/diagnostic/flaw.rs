3e53e2eb9bb18a63b4fe7c5f0dd3c61c8b02947b
patrickett <patrickett@protonmail.com>
2026-01-21 20:36:42 -0500

    Update flaw.rs

--- ginc/src/diagnostic/flaw.rs
+++ ginc/src/diagnostic/flaw.rs
@@ -1,8 +1,10 @@
+use std::{fs, ops::Range, path::PathBuf};
+
 use crate::{diagnostic::Printable, frontend::Token};
 
 #[derive(Debug)]
-pub enum GincFlaw {
-    ParseError(Vec<chumsky::prelude::Rich<'static, Token<'static>>>),
+pub enum GincFlaw<'a> {
+    ParseError(Vec<(Vec<Rich<'a, Token<'a>>>, PathBuf)>),
     TypeError(String),
     BorrowError(String),
     IoError(String),
@@ -23,13 +25,34 @@ macro_rules! flaw {
 }
 
 // Implement Printable for GincFlaw
-impl Printable for GincFlaw {
+impl Printable for GincFlaw<'_> {
     fn print(&self) {
         match self {
             GincFlaw::ParseError(errors) => {
-                eprintln!("Parse Error:");
-                for error in errors {
-                    flaw!("- {:#?}", error);
+                for (errors, path) in errors {
+                    let filename = path.to_str().expect("msg").to_string();
+                    let src_txt = fs::read_to_string(path).expect("msg");
+
+                    let mut cache = ariadne::sources([(filename.clone(), src_txt)]);
+
+                    for err in errors.iter() {
+                        let span = err.span();
+                        let start = span.start();
+                        let end = span.end();
+
+                        let ariadne_span = (filename.clone(), Range { start, end });
+                        let msg = format!("{:?}", err);
+
+                        let report = Report::build(
+                            ReportKind::Custom("flaw", ariadne::Color::Red),
+                            ariadne_span.clone(),
+                        )
+                        .with_message(msg)
+                        .with_label(Label::new(ariadne_span).with_message("here"))
+                        .finish();
+
+                        report.eprint(&mut cache).unwrap();
+                    }
                 }
             }
             GincFlaw::TypeError(msg) => {
@@ -42,5 +65,40 @@ impl Printable for GincFlaw {
                 flaw!("IO Error: {}", msg);
             }
         }
+    }
+}
+
+use ariadne::{Label, Report, ReportKind};
+use chumsky::{error::Rich, span::Span};
+
+/// Print a list of `chumsky` parse errors using ariadne for coloured output.
+///
+/// This helper is meant to be called from the parser when it encounters
+/// errors.  It takes ownership of the error vector and prints each one to
+/// stderr.
+pub fn print_parse_errors(
+    errors: &Vec<chumsky::prelude::Rich<'static, Token<'static>>>,
+    src_txt: &str,
+    filename: String,
+) {
+    let mut cache = ariadne::sources([(filename.clone(), src_txt)]);
+
+    for err in errors.iter() {
+        let span = err.span();
+        let start = span.start();
+        let end = span.end();
+
+        let ariadne_span = (filename.clone(), Range { start, end });
+        let msg = format!("{:?}", err);
+
+        let report = Report::build(
+            ReportKind::Custom("flaw", ariadne::Color::Red),
+            ariadne_span.clone(),
+        )
+        .with_message(msg)
+        .with_label(Label::new(ariadne_span).with_message("here"))
+        .finish();
+
+        report.eprint(&mut cache).unwrap();
     }
 }
